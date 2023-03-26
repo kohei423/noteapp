d4b1fcebdd7a569d84a5b409822b8578eb1acb68
kohei423 <kyamauch5476@gmail.com>
2023-03-26 11:03:45 +0900

    メモアプリ実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import uuid from 'react-uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  const [notes,setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote,setActiveNote] = useState([]);
+
+  useEffect(()=> {
+    localStorage.setItem("notes",JSON.stringify(notes));
+  },[notes])
+
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  },[])
+
+  const onAddNote = () => {
+    console.log("test");
+    const newNote = {
+      id : uuid(),
+      title : "新しいノート",
+      content: "内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === updatedNote.id){
+        return updatedNote;
+      }else{
+        return note;
+      }
+    });
+
+    setNotes(updatedNotesArray);
+  }
+ 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote} />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
     </div>
   )
 }
